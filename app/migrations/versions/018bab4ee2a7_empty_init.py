142926328681f5a7e314d1e1ce36df637ac22a56
Tonny Mulli <tonnymulli2118@gmail.com>
2024-08-02 14:18:36 +0300

    code along

---
+++ app/migrations/versions/018bab4ee2a7_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 018bab4ee2a7
+Revises: 
+Create Date: 2024-08-02 14:15:12.113872
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '018bab4ee2a7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
